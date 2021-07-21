9763ddfbf5f9c7c4feab12e4dd6ec987fc14eb7d
jonatasvenancio167 <jvenancio167@hotmail.com>
2021-07-20 21:20:35 -0300

    Configurando o routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'laboratories/index'
+  # get 'laboratories/show'
+  # get 'laboratories/create'
+  # get 'laboratories/update'
+  # get 'laboratories/destroy'
+  # get 'exams/index'
+  # get 'exams/show'
+  # get 'exams/create'
+  # get 'exams/update'
+  # get 'exams/destroy'
 
-  namespace :api do
-    namespace :v1 do
-      resources :exams
-      resources :laboratories
-    end
-  end
+  resources :exams
+  resources :laboratories
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
